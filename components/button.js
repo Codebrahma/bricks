b2c618d036dde0f979b17587f59b3638a974228c
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2020-02-18 18:17:55 +0530

    Added layout to input and button

--- components/button.js
+++ components/button.js
@@ -1,22 +1,35 @@
-import styled from '@emotion/styled'
-import css from '@styled-system/css'
-import { space, color, typography, border, borderColor, borderRadius } from 'styled-system'
-import PTypes from 'prop-types';
-import propTypes from '@styled-system/prop-types'
+import styled from "@emotion/styled";
+import css from "@styled-system/css";
+import {
+  space,
+  color,
+  typography,
+  border,
+  borderColor,
+  borderRadius,
+  layout
+} from "styled-system";
+import PTypes from "prop-types";
+import propTypes from "@styled-system/prop-types";
 
 export const OutlinedButton = styled.button(
   css({
-    appearance: 'none',
-    fontFamily: 'body',
-    fontSize: 'desktop.1',
-    border: '3px solid',
-    borderColor: 'black.1',
-    color: 'black.1',
-    bg: 'tint',
-    p: '5px 40px',
+    appearance: "none",
+    fontFamily: "body",
+    fontSize: "desktop.1",
+    border: "3px solid",
+    borderColor: "black.1",
+    color: "black.1",
+    bg: "tint",
+    p: "5px 40px"
   }),
-  color, space, typography,
-  border, borderColor, borderRadius
+  color,
+  space,
+  typography,
+  border,
+  borderColor,
+  borderRadius,
+  layout
 );
 
 InputButton.propTypes = {
@@ -26,26 +39,32 @@ InputButton.propTypes = {
   ...propTypes.typography,
   ...propTypes.space,
   ...propTypes.borderRadius,
+  ...propTypes.layout
 };
 
-export const InputButton = styled('input')(
+export const InputButton = styled("input")(
   css({
-    appearance: 'none',
-    fontFamily: 'body',
-    fontSize: 'desktop.1',
-    border: '2px solid',
-    borderColor: 'black.1',
-    color: 'tint',
-    bg: 'black.1',
-    p: '5px',
+    appearance: "none",
+    fontFamily: "body",
+    fontSize: "desktop.1",
+    border: "2px solid",
+    borderColor: "black.1",
+    color: "tint",
+    bg: "black.1",
+    p: "5px"
   }),
-  color, space, typography,
-  border, borderColor, borderRadius
-)
-InputButton.defaultProps = {type: 'submit'}
+  color,
+  space,
+  typography,
+  border,
+  borderColor,
+  borderRadius,
+  layout
+);
+InputButton.defaultProps = { type: "submit" };
 InputButton.propTypes = {
   type: PTypes.string
-}
+};
 
 InputButton.propTypes = {
   ...propTypes.border,
@@ -54,10 +73,10 @@ InputButton.propTypes = {
   ...propTypes.typography,
   ...propTypes.space,
   ...propTypes.borderRadius,
-}
-
+  ...propTypes.layout
+};
 
 export const EmptyButton = styled.button`
   background: none;
   border: none;
-`
+`;
--- components/inputText.js
+++ components/inputText.js
@@ -1,24 +1,39 @@
-import styled from '@emotion/styled'
-import css from '@styled-system/css'
-import { space, color, typography, border, borderColor, borderRadius, backgroundColor } from 'styled-system'
-import propTypes from '@styled-system/prop-types'
+import styled from "@emotion/styled";
+import css from "@styled-system/css";
+import {
+  space,
+  color,
+  typography,
+  border,
+  borderColor,
+  borderRadius,
+  backgroundColor,
+  layout
+} from "styled-system";
+import propTypes from "@styled-system/prop-types";
 
-export const InputText = styled('input')(
+export const InputText = styled("input")(
   css({
-    appearance: 'none',
-    fontFamily: 'body',
-    fontSize: 'desktop.1',
-    border: '2px solid',
-    borderColor: 'black.1',
-    color: 'black.1',
-    bg: 'tint',
-    p: '12px',
-    borderRadius: 1,
+    appearance: "none",
+    fontFamily: "body",
+    fontSize: "desktop.1",
+    border: "2px solid",
+    borderColor: "black.1",
+    color: "black.1",
+    bg: "tint",
+    p: "12px",
+    borderRadius: 1
   }),
-  space, color, typography,
-  border, borderColor, borderRadius, backgroundColor,
-)
-InputText.defaultProps = {type: 'text'}
+  space,
+  color,
+  typography,
+  border,
+  borderColor,
+  borderRadius,
+  backgroundColor,
+  layout
+);
+InputText.defaultProps = { type: "text" };
 InputText.propTypes = {
   ...propTypes.border,
   ...propTypes.borderColor,
@@ -26,4 +41,5 @@ InputText.propTypes = {
   ...propTypes.color,
   ...propTypes.typography,
   ...propTypes.backgroundColor,
-}
+  ...propTypes.layout
+};
