cda47db0495b8741b7ee2758665a027af50f235e
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2020-02-18 17:49:44 +0530

    Made border radius and background color for input text field customizable

--- components/inputText.js
+++ components/inputText.js
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled'
 import css from '@styled-system/css'
-import { space, color, typography, border, borderColor } from 'styled-system'
+import { space, color, typography, border, borderColor, borderRadius, backgroundColor } from 'styled-system'
 import propTypes from '@styled-system/prop-types'
 
 export const InputText = styled('input')(
@@ -12,15 +12,18 @@ export const InputText = styled('input')(
     borderColor: 'black.1',
     color: 'black.1',
     bg: 'tint',
-    p: '5px',
+    p: '12px',
+    borderRadius: 1,
   }),
   space, color, typography,
-  border, borderColor
+  border, borderColor, borderRadius, backgroundColor,
 )
 InputText.defaultProps = {type: 'text'}
 InputText.propTypes = {
   ...propTypes.border,
   ...propTypes.borderColor,
+  ...propTypes.borderRadius,
   ...propTypes.color,
   ...propTypes.typography,
+  ...propTypes.backgroundColor,
 }
